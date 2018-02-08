a4989b585231e3ccdc0fd7e2ee27cfcb5dc9e991
brendon-ai <brendon-m@outlook.com>
2018-02-08 12:52:15 -0500

    Added support for multiple input examples; neural net-does-not-work

--- WWDC2018Playground.playground/Sources/NeuralNetwork.swift
+++ WWDC2018Playground.playground/Sources/NeuralNetwork.swift
@@ -33,22 +33,41 @@ public struct NeuralNetwork {
         self.weightMatrixShapes = weightMatrixShapes
     }
     
-    // Run inference using a single-dimensional floating-point matrix as input
-    public func infer(input: [Float]) -> [Float] {
-        // Make sure that the length of the input is the same as the width of the first weight matrix
-        precondition(input.count == weightMatrixShapes[0].0, "Invalid input shape")
-        // Create an array to hold the output of one layer at a time as they are executed; initialize it with the input
-        var workingOutput = input
+    // Run inference using an array of floating-point arrays, each of which is one input
+    public func infer(input: [[Float]]) -> [[Float]] {
+        // Append each of the input arrays to a single-dimensional array that can be used with Accelerate
+        let inputSingleDimensionalArray = input.reduce([], +)
+        // Create an array to hold the output of one layer at a time as they are executed; initialize it with the transpose of the input array
+        let numExamples = input.count
+        let exampleLength = input[0].count
+        var workingOutput = [Float](repeating: 0, count: numExamples * exampleLength)
+        vDSP_mtrans(inputSingleDimensionalArray, 1, &workingOutput, 1, vDSP_Length(exampleLength), vDSP_Length(numExamples))
+        print(workingOutput)
         // For each of the weight matrices (represented as one-dimensional arrays) and their corresponding shapes
         for (weightMatrix, shape) in zip(weightMatrices, weightMatrixShapes) {
-            // The length of the output column vector is equal to the height of the weight matrix
-            var output = [Float](repeating: 0, count: shape.1)
+            print(shape)
+            // The length of the output column vector is equal to the height of the weight matrix times the number of input examples
+            var output = [Float](repeating: 0, count: shape.1 * numExamples)
             // Multiply the weight matrix by the current working output as a row vector
-            vDSP_mmul(weightMatrix, 1, workingOutput, 1, &output, 1, vDSP_Length(shape.0), 1, vDSP_Length(output.count))
+            vDSP_mmul(weightMatrix, 1, workingOutput, 1, &output, 1, vDSP_Length(shape.0), vDSP_Length(exampleLength), vDSP_Length(output.count))
             // Update the working output with this value
             workingOutput = output
+            print(workingOutput)
         }
-        // Return the final working output, which is the output of the network
-        return workingOutput
+        // Transpose the final working output so that it can be divided into output arrays for each example
+        var outputTranspose = [Float](repeating: 0, count: workingOutput.count)
+        vDSP_mtrans(workingOutput, 1, &outputTranspose, 1, vDSP_Length(numExamples), vDSP_Length(exampleLength))
+        // Create an output array of arrays to add the example outputs to
+        var outputExamples = [[Float]]()
+        // Stride over the length of the output array by the length of the output for one example
+        let outputExampleLength = outputTranspose.count / numExamples
+        for exampleStartIndex in stride(from: 0, to: outputTranspose.count, by: outputExampleLength) {
+            // Get the range of the transposed array from the starting index to the starting index plus the length of an example
+            let outputExample = outputTranspose[exampleStartIndex..<exampleStartIndex + outputExampleLength]
+            // Append the list to the list of output examples
+            outputExamples.append(Array(outputExample))
+        }
+        // Return the list of output examples
+        return outputExamples
     }
 }
